1a20c0ab47174a6aca69cf4bcea1a6f30664cc47
AnthonyHamon <anthony.hamon91@gmail.com>
2023-06-20 12:07:19 +0200

    css and render function created by chatGPT

---
+++ script.js
@@ -0,0 +1,77 @@
+let fields = [
+    null,
+    'circle',
+    null,
+    null,
+    null,
+    'cross',
+    null,
+    null,
+    null,
+]
+
+function init(){
+    render();
+}
+
+function render() {
+    const contentDiv = document.getElementById('content');
+    let tableHTML = '<table>';
+
+    for (let i = 0; i < 3; i++) {
+      tableHTML += '<tr>';
+
+      for (let j = 0; j < 3; j++) {
+        const index = i * 3 + j;
+        let symbol = '';
+
+        if (fields[index] === 'circle') {
+          symbol = generateCircleSVG();
+        } else if (fields[index] === 'cross') {
+          symbol = generateCrossSVG();
+        }
+
+        tableHTML += `<td>${symbol}</td>`;
+      }
+
+      tableHTML += '</tr>';
+    }
+
+    tableHTML += '</table>';
+    contentDiv.innerHTML = tableHTML;
+  }
+
+  function generateCircleSVG() {
+    const color = '#00B0EF';
+    const width = 70;
+    const height = 70;
+
+    return `<svg width="${width}" height="${height}">
+              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.5s" fill="freeze" />
+              </circle>
+            </svg>`;
+}
+
+  function generateCrossSVG() {
+    const color = '#FFC000';
+    const width = 70;
+    const height = 70;
+
+    const svgHtml = `
+      <svg width="${width}" height="${height}">
+        <line x1="0" y1="0" x2="${width}" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="0; ${width}" dur="500ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="500ms" />
+        </line>
+        <line x1="${width}" y1="0" x2="0" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="${width}; 0" dur="500ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="500ms" />
+        </line>
+      </svg>
+    `;
+
+    return svgHtml;
+}
